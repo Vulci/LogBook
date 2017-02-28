23139f903f76919bf608572ef2959184689b2e16
Vulci <botosakos88@gmail.com>
2017-02-28 23:32:44 +0100

    +1

--- src/main/java/com/logbook/controller/TestController.java
+++ src/main/java/com/logbook/controller/TestController.java
@@ -1,16 +1,17 @@
 package com.logbook.controller;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class TestController {
 	
-	@RequestMapping("/ping")
-	public String ping(){
-		return "Pong!";
+	@RequestMapping("/greeting")
+	public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model){
+		model.addAttribute("name", name);
+		return "greeting";
 	}
 	
 }
